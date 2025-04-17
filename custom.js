f8dedd35b50d6832811e41d167a9efb98b54fae5
Andrew <andrewfirmansap@gmail.com>
2025-04-17 09:58:59 +0700

    add(Home): dynamic occupation

--- custom.js
+++ custom.js
@@ -42,3 +42,62 @@ $(document).ready(function () {
         $("#ubs_exp_penjelasan").toggle();
     });
 });
+
+var _CONTENT = [
+    'Software Engineer',
+    'Web Developer',
+    'Backend Developer',
+    'PHP Developer',
+    'SilverStripe Specialist',
+    'MySQL Wrangler',
+    'API Integrator',
+    'System Debugger',
+    'JavaScript Explorer',
+    'RESTful API Crafter',
+    'Side Project Dreamer',
+    'Code Refactorer',
+    'SQL Query Tuner',
+    'Tech Learner',
+    'AI Enthusiast',
+    'Clean Code Advocate',
+    'Gamer',
+    'Digital Minimalist',
+    'Problem Solver',
+    'Performance Tweaker',
+    'Framework Navigator',
+    'Codebase Archaeologist'
+  ];
+  
+var _PART = 0;
+var _PART_INDEX = 0;
+var _INTERVAL_VAL;
+var _ELEMENT = document.querySelector("#i-am-a-block");
+function Type() {
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text + " _";
+    _PART_INDEX++;
+    if (text === _CONTENT[_PART]) {
+        clearInterval(_INTERVAL_VAL);
+        setTimeout(function () {
+            _INTERVAL_VAL = setInterval(Delete, 30);
+        }, 1000);
+    }
+}
+function Delete() {
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text + " _";
+    _PART_INDEX--;
+    if (text === '') {
+        clearInterval(_INTERVAL_VAL);
+        if (_PART == (_CONTENT.length - 1))
+            _PART = 0;
+        else
+            _PART++;
+        _PART_INDEX = 0;
+        setTimeout(function () {
+            _INTERVAL_VAL = setInterval(Type, 80);
+        }, 100);
+    }
+}
+_INTERVAL_VAL = setInterval(Type, 80);
+
